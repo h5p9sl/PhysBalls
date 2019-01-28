a72c0766544e93d2c1a09d051c4c0df2460e02e5
h5p9sl <21267024+h5p9sl@users.noreply.github.com>
2019-01-27 21:27:36 -0700

    Cleaned up some code and added some documentation for Ball::update

--- Ball.cpp
+++ Ball.cpp
@@ -37,30 +37,44 @@ void Ball::randomizePosition()
     this->m_shape.setPosition(this->m_position);
 }
 
+// Called once every frame before being drawn.
 void Ball::update(float f_delta) {
-    m_shape.setRadius(this->m_mass * 10.f);
-    float circleRadius = this->m_shape.getRadius();
-    m_shape.setOrigin(circleRadius, circleRadius);
-
-    if (!this->m_static) this->m_position += this->m_velocity;
+    
+    // FIXME: This shouldn't really be here
+    if (this->m_static) {
+        this->m_shape.setRadius(this->m_mass * 10.f);
+        float circleRadius = this->m_shape.getRadius();
+        m_shape.setOrigin(circleRadius, circleRadius);
+    }
+    else {
+        // Apply velocity to position
+        this->m_position += this->m_velocity;
+    }
+    // Update position
     this->m_shape.setPosition(this->m_position);
 
     for (auto object : globals->game.objects) {
+
         if (object != this) {
 
             sf::Vector2f vectorToObject = { this->m_position - object->m_position };
             sf::Vector2f vectorToThis = { object->m_position - this->m_position };
             float distance = sqrtf( powf(vectorToObject.x, 2) + powf(vectorToObject.y, 2) );
+
             // Normalize vectors
             vectorToObject /= distance;
             vectorToThis /= distance;
 
             float distance_frm_surfaces = distance - this->getRadius() - object->getRadius();
-
-            if (distance <= this->m_shape.getRadius() + object->m_shape.getRadius())
+            
+            // Collision detection
+            if (distance_frm_surfaces <= 0.f)
             {
+                // NOTE: From this point on, stuff stops making sense.
+
                 sf::Vector2f oVeloc[] = { this->m_velocity, object->m_velocity };
 
+                // If the other object doesn't obey the laws of physics :P
                 if (object->m_static == true) {
                     // move *this out of the circumference of the other shape
                     this->m_position += vectorToObject;
@@ -71,39 +85,21 @@ void Ball::update(float f_delta) {
                 this->m_position += vectorToObject / this->m_mass;
                 object->m_position += vectorToThis / object->m_mass;
 
+                // Apply the same amount of velocity used when colliding with the object
                 this->m_velocity = vectorToObject / this->m_mass;
                 object->m_velocity = vectorToThis / object->m_mass;
 
+                // Add original velocity for some reason, idk
                 this->m_velocity.x += oVeloc[0].x;
                 this->m_velocity.y += oVeloc[0].y;
                 object->m_velocity.x += oVeloc[1].x;
                 object->m_velocity.y += oVeloc[1].y;
-
             }
             else {
+                // No collision detected;
+                // Attract this object to other objects using some fancy math that I made up
                 this->m_velocity -= vectorToObject * object->m_mass / distance / this->m_mass;
             }
-
-            sf::Vector2f windowSize = globals->game.window->getView().getSize();
-            const float radius = this->m_shape.getRadius();
-
-            // if (this->m_position.x + radius > windowSize.x) {
-            //     this->m_position.x = windowSize.x - radius;
-            //     this->m_velocity.x = -this->m_velocity.x * 0.25f;
-            // }
-            // if (this->m_position.y + radius > windowSize.y) {
-            //     this->m_position.y = windowSize.y - radius;
-            //     this->m_velocity.y = -this->m_velocity.y * 0.25f;
-            // }
-            // if (this->m_position.x - radius < 0.f) {
-            //     this->m_position.x = 0.f + radius;
-            //     this->m_velocity.x = -this->m_velocity.x * 0.25f;
-            // }
-            // if (this->m_position.y - radius < 0.f) {
-            //     this->m_position.y = 0.f + radius;
-            //     this->m_velocity.y = -this->m_velocity.y * 0.25f;
-            // }
-
         }
     }
 }
